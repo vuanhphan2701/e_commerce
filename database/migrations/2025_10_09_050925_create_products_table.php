5d98e929d684d7b6f8839ccc92d7ada2e1e24e16
Vũ Anh Phan <pavu2701@gmail.com>
2025-10-11 01:11:47 +0700

    fix: delete field email in product migrations

--- database/migrations/2025_10_09_050925_create_products_table.php
+++ database/migrations/2025_10_09_050925_create_products_table.php
@@ -14,7 +14,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
             $table->text('description')->nullable();
             $table->decimal('price', 8, 2);
             $table->timestamps();
